3f986316ac8c5ef5205290e3005c9a104c696ea9
Konstantinjj <kohctahtuh.pog@yandex.ru>
2024-05-25 02:21:27 +0300

    styles

--- CarServiceCRM/cars/forms.py
+++ CarServiceCRM/cars/forms.py
@@ -5,3 +5,19 @@ class CarForm(forms.ModelForm):
     class Meta:
         model = Car
         fields = ['gos_num', 'vin_number', 'brand', 'model', 'year_of_production', 'client']
+        labels = {
+            'gos_num': 'Гос номер',
+            'vin_number': 'ВИН номер',
+            'brand': 'Марка',
+            'model': 'Модель',
+            'year_of_production': 'Год выпуска',
+            'client': 'Клиент'
+        }
+        widgets = {
+            'gos_num': forms.TextInput(attrs={'class': 'form-control form-control-half'}),
+            'vin_number': forms.TextInput(attrs={'class': 'form-control form-control-half'}),
+            'brand': forms.TextInput(attrs={'class': 'form-control form-control-half'}),
+            'model': forms.TextInput(attrs={'class': 'form-control form-control-half'}),
+            'year_of_production': forms.NumberInput(attrs={'class': 'form-control form-control-half'}),
+            'client': forms.Select(attrs={'class': 'form-control form-control-half'}),
+        }
--- CarServiceCRM/clients/forms.py
+++ CarServiceCRM/clients/forms.py
@@ -1,8 +1,19 @@
 from django import forms
 from .models import Client
 
-
 class ClientForm(forms.ModelForm):
     class Meta:
         model = Client
         fields = ['first_name', 'last_name', 'middle_name', 'phone_number']
+        labels = {
+            'first_name': 'Имя',
+            'last_name': 'Фамилия',
+            'middle_name': 'Отчество',
+            'phone_number': 'Номер телефона'
+        }
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control form-control-half'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control form-control-half'}),
+            'middle_name': forms.TextInput(attrs={'class': 'form-control form-control-half'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control form-control-half'}),
+        }
