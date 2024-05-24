19ea3c474d3fd2bb5fba597b1cd9c37fc6bca214
Konstantinjj <kohctahtuh.pog@yandex.ru>
2024-05-25 01:31:44 +0300

    Client functional add

--- CarServiceCRM/CarServiceCRM/urls.py
+++ CarServiceCRM/CarServiceCRM/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('clients/', include('clients.urls')),
+    path('cars/', include('cars.urls')),
 ]
--- CarServiceCRM/main/views.py
+++ CarServiceCRM/main/views.py
@@ -2,9 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
-
-def clients(request):
-    return render(request, 'main/clients.html')
-
-def cars(request):
-    return render(request, 'main/cars.html')
