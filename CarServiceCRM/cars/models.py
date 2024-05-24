3103603aa06df8695d52bc2ba0de7911c6922165
Konstantinjj <kohctahtuh.pog@yandex.ru>
2024-05-25 01:32:30 +0300

    Client functional add

---
+++ CarServiceCRM/cars/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from clients.models import Client
+
+
+class Car(models.Model):
+    gos_num = models.CharField(max_length=20)
+    vin_number = models.CharField(max_length=17)
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year_of_production = models.IntegerField()
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='cars')
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.license_plate})"
---
+++ CarServiceCRM/clients/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Client
+from .forms import ClientForm
+
+def client_list(request):
+    clients = Client.objects.all().prefetch_related('cars')
+    return render(request, 'clients/client_list.html', {'clients': clients})
+
+
+def client_create(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm()
+    return render(request, 'clients/client_form.html', {'form': form})
