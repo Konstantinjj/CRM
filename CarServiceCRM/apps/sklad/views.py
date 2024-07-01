9552ca36d46cf73b2fdebb01831ecd458650feda
Konstantinjj <kohctahtuh.pog@yandex.ru>
2024-07-01 14:05:42 +0300

    Sklad add position

--- CarServiceCRM/apps/sklad/views.py
+++ CarServiceCRM/apps/sklad/views.py
@@ -3,32 +3,44 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Item, SortGroup
 from .forms import ItemForm
 
+
 class OverviewListView(ListView):
     model = Item
     template_name = 'sklad/overview.html'
     context_object_name = 'items'
+    paginate_by = 10
 
     def get_queryset(self):
         query = self.request.GET.get('q')
         group_id = self.request.GET.get('group')
 
-        if group_id and query:
-            queryset = Item.objects.filter(sort_group__id=group_id, name__icontains=query).order_by('-id')
-        elif group_id:
-            queryset = Item.objects.filter(sort_group__id=group_id).order_by('-id')
-        elif query:
-            queryset = Item.objects.filter(name__icontains=query).order_by('-id')
-        else:
-            queryset = Item.objects.all().order_by('-id')
+        if not group_id:
+            first_group = SortGroup.objects.first()
+            if first_group:
+                group_id = first_group.id
 
-        return queryset
+        if query:
+            return Item.objects.filter(name__icontains=query).order_by('-id')
+        elif group_id:
+            try:
+                group_id = int(group_id)
+                return Item.objects.filter(sort_group__id=group_id).order_by('-id')
+            except ValueError:
+                return Item.objects.none()
+        else:
+            return Item.objects.none()
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['query'] = self.request.GET.get('q', '')
         context['sort_groups'] = SortGroup.objects.all()
         context['active_group_id'] = self.request.GET.get('group')
+        if not context['active_group_id'] and not context['query']:
+            first_group = SortGroup.objects.first()
+            if first_group:
+                context['active_group_id'] = first_group.id
         return context
+
 
 class ItemCreateView(CreateView):
     model = Item
@@ -36,11 +48,13 @@ class ItemCreateView(CreateView):
     template_name = 'sklad/item_form.html'
     success_url = reverse_lazy('overview')
 
+
 class ItemUpdateView(UpdateView):
     model = Item
     form_class = ItemForm
     template_name = 'sklad/item_form.html'
     success_url = reverse_lazy('overview')
+
 
 class ItemDeleteView(DeleteView):
     model = Item
